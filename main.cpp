e4619637d565a8a67bb0daa94e17718bf0f27cf8
SeriousHyena <emackiii@gmail.com>
2021-10-14 16:15:56 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+*File name: ThreeTimesN2
+* -> Create a function called threeTimesN
+* -> It takes two integer parameters
+* -> first parameter, input is an integer passed by value
+* -> The second parameter, output is an integer passed by reference
+* -> Store the input, multiply by three into theoutput variable
+* -> call the function and print the results
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
