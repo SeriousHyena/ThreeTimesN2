532188114941805c713d99e475ba4a9b7f132e14
SeriousHyena <emackiii@gmail.com>
2021-10-15 05:40:18 -0700

    progress

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 * -> It takes two integer parameters
 * -> first parameter, input is an integer passed by value
 * -> The second parameter, output is an integer passed by reference
-* -> Store the input, multiply by three into theoutput variable
+* -> Store the input, multiply by three into the output variable
 * -> call the function and print the results
 */
 
@@ -12,8 +12,17 @@
 
 using namespace std;
 
+//Declare functions
+void threeTimesN(int aVal, int & aRef );
+
 int main()
 {
+	void threeTimesN(int aVal, int& aRef);
 
 	return 0;
+}
+
+void threeTimesN(int aVal, int& aRef)
+{
+
 }
